3562108622e978437f3fbcca8f32844329418380
courtney <coa.mcallister@gmail.com>
2017-02-02 20:47:23 -0500

    create the javascript literal that is managing the application. This object is currently caching, rendering, and managing todo list state

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,30 @@
-console.log('running');
+var todoApp = {
+  todos: [], //data source/store
+  init: function(){
+    this.cacheDom();
+    this.addEventListeners();
+    this.render();
+  },
+  cacheDom: function(){
+    this.createButton = document.querySelector('#create');
+    this.taskInput = document.querySelector('#task');
+    this.list = document.querySelector('#list');
+  },
+  render: function(){
+    var listItemsFromTodos = this.todos.map(function(todo){
+      return '<li>' + todo + '</li>';
+    }).join('');
+    this.list.innerHTML = listItemsFromTodos;
+  },
+  addEventListeners: function() {
+    this.createButton.addEventListener('click', this.addTodo);
+  },
+  addTodo: function(){
+    var taskValue = todoApp.taskInput.value;
+    todoApp.todos.push(taskValue);
+    todoApp.render();
+  }
+};
+// console.log(todoApp);
+todoApp.init();
+// console.log(todoApp);
